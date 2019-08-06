8e9b0ffb659c527ac71aff85c48d5d2be51937ea
Chansamnang <limchansamnang@gmail.com>
2019-08-06 11:15:09 +0700

    Start service to play music

---
+++ Services/app/src/main/java/com/example/services/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.services
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun demo(v:View){
+        var i = Intent(this,MyMusicService::class.java)
+        when(v.id){
+            R.id.start -> {
+                startService(i)
+            }
+            R.id.stop -> {
+                stopService(i)
+            }
+            R.id.startNew -> {
+                startActivity(Intent(this,Main2Activity::class.java))
+            }
+        }
+    }
+}
